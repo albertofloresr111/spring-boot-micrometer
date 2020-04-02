d3fb6aa74d712274259f471440c274fa09454d14
alberto.floresr111 <alberto.floresr111@gmail.com>
2020-04-01 20:10:55 -0600

    Using prometheus to monitorize spring boot applications v1

---
+++ src/main/java/com/bitacora/it/micrometer/MicrometerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bitacora.it.micrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bitacora/it/micrometer/controller/TiendaOnlineController.java
@@ -0,0 +1,45 @@
+package com.bitacora.it.micrometer.controller;
+
+import io.micrometer.core.annotation.Timed;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ventas")
+public class TiendaOnlineController {
+
+    @Timed(
+            value = "bitacora.deportes.request1",
+            histogram = true,
+            percentiles = {0.95,0.99},
+            extraTags ={"version","1.0"}
+    )
+    @GetMapping("/balones")
+    public String hello()  {
+        return "Un artículo Deportivo vendido";
+    }
+
+    @Timed(
+            value = "bitacora.computacion.request2",
+            histogram = true,
+            percentiles = {0.95,0.99},
+            extraTags ={"version","1.0"}
+    )
+    @GetMapping("/mause")
+    public String hello3()  {
+        return "Artículo de Computacion vendido";
+    }
+
+    @Timed(
+            value = "bitacora.computacion.request2",
+            histogram = true,
+            percentiles = {0.95,0.99},
+            extraTags ={"version","1.0"}
+    )
+    @GetMapping("/laptop")
+    public String hello4()  {
+        return "Artículo de Computacion vendido";
+    }
+
+}
